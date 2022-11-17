c51fd7db4de424ba2b0ec0157a2eaea34462b8d6
David <24713582+Davbp@users.noreply.github.com>
2022-11-17 10:13:32 +0100

    Delete ejercicio_6 directory

--- ejercicio_6/ejercicio_6_cursos/src/main/java/com/curso/dao/CursosDao.java
+++
@@ -1,9 +0,0 @@
-package com.curso.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.curso.model.Curso;
-
-public interface CursosDao extends JpaRepository<Curso, String> {
-
-}
